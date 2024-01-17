3b1d0ea3df1d075b397aba27ffc48cd0678b306b
Yrwom <139040533+Yrwom@users.noreply.github.com>
2024-01-17 01:01:19 +0000

    Created Program

--- .ccls-cache/@home@runner@Lyrics/main.c
+++ .ccls-cache/@home@runner@Lyrics/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  printf("Unit Test #1 Expected Outcome\nOh, well imagine /\nAs I'm pacing the pews in a church corridor /\nAnd I can't help but to hear /\nNo, I can't help but to hear an exchanging of words...\n \n");
+  printf("Unit Test #2 Expected Outcome\nOh-oh-oh-oh-oh,\noh-oh-oh-oh,\noh-oh-oh /\nCaught in a bad romance...\n \n");
+  printf("Unit Test #3 Expected Outcome\nComing out of my cage\nAnd I've been doing just fine\nGotta gotta be down\nBecause i want it all\n \n");
+  printf("Unit Test #4 Expected Outcome\nNow, this is a story all about how\nMy life got flipped-turned upside down\nAnd I'd like to take a minute\nJust sit right there\nI'll tell you how I became the prince of a town called Bel-Air\n \n");
   return 0;
 }
